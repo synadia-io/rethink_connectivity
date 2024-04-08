37d329bf3cfb86f76b7d9b9f40064bfaceaf5157
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-04-08 12:49:35 -0700

    Making more progress and the architecture is starting to feel good in my head

--- 18-benthos-and-nats/db/schema.sql
+++ 18-benthos-and-nats/db/schema.sql
@@ -8,27 +8,10 @@ CREATE TABLE customers (
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE products (
-  id TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
-  description TEXT,
-  price REAL NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TRIGGER update_time_customers
   BEFORE UPDATE 
   ON customers
   FOR EACH ROW
 BEGIN 
   UPDATE customers SET updated_at = CURRENT_TIMESTAMP WHERE id = OLD.id; 
-END;
-
-CREATE TRIGGER update_time_products
-  BEFORE UPDATE 
-  ON products
-  FOR EACH ROW
-BEGIN 
-  UPDATE products SET updated_at = CURRENT_TIMESTAMP WHERE id = OLD.id; 
 END;
