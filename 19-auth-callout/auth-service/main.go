18d349c778cd28c654aa139a5b0c24897b57919f
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-06-19 15:34:49 -0700

    Added comments

--- 19-auth-callout/auth-service/main.go
+++ 19-auth-callout/auth-service/main.go
@@ -10,6 +10,8 @@ import (
 // Warning! You will want this in a secret store
 // of some kind instead of the source code like this
 const NKeySeed string = "SAAIRUPUPZ4CZZX4EYX2MF6A3KN7FGI3AQPEVF3HI2JXVNV6DJGSTZGDFE"
+
+// Use your own google app client ID here
 const ClientID = "476732082534-gpp6r1t67lirjfddjckm81vr6c2kikr1.apps.googleusercontent.com"
 
 func main() {
