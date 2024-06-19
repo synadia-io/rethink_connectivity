7469c5ab0982e748fb42455441a75220612f6d9c
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-06-19 15:09:30 -0700

    Adding some finishing touches

---
+++ 19-auth-callout/auth-service/auth_service.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"runtime"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/micro"
+	"github.com/nats-io/nkeys"
+)
+
+type GoogleClaims struct {
+	Name          string `json:"name"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	Picture       string `json:"picture"`
+}
+
+type AuthService struct {
+	issuerKeyPair nkeys.KeyPair
+	verifier      *oidc.IDTokenVerifier
+	clientID      string
+
+	username string
+	password string
+}
+
+func NewAuthService(issuer nkeys.KeyPair, clientID string, username string, password string) *AuthService {
+	return &AuthService{
+		issuerKeyPair: issuer,
+		clientID:      clientID,
+		username:      username,
+		password:      password,
+	}
+}
+
+func (a *AuthService) Run(ctx context.Context) error {
+	// Initialize OIDC Provider
+	provider, err := oidc.NewProvider(ctx, "https://accounts.google.com")
+	if err != nil {
+		return err
+	}
+	a.verifier = provider.Verifier(&oidc.Config{ClientID: ClientID})
+
+	// Connect to NATS
+	nc, err := nats.Connect(nats.DefaultURL, nats.UserInfo("auth", "auth"))
+	if err != nil {
+		return err
+	}
+
+	// Add Service
+	_, err = micro.AddService(nc, micro.Config{
+		Name:        "auth",
+		Version:     "0.0.1",
+		Description: "Handle authorization of Google JWTs for chat applications",
+		Endpoint: &micro.EndpointConfig{
+			Subject: "$SYS.REQ.USER.AUTH",
+			Handler: a,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	log.Println("listening on $SYS.REQ.USER.AUTH")
+	runtime.Goexit()
+	return nil
+}
+
+func (a *AuthService) Handle(r micro.Request) {
+	log.Println("Received Request")
+
+	rc, err := jwt.DecodeAuthorizationRequestClaims(string(r.Data()))
+	if err != nil {
+		log.Println("Error", err)
+		r.Error("500", err.Error(), nil)
+	}
+
+	userNkey := rc.UserNkey
+	serverId := rc.Server.ID
+	claims := jwt.NewUserClaims(rc.UserNkey)
+	claims.Audience = "$G"
+
+	// this gives me a backdoor with the CLI. Don't do this in production!
+	if rc.ConnectOptions.Username == "cli" && rc.ConnectOptions.Password == "my-password" {
+		claims.Name = rc.ConnectOptions.Username
+		claims.Permissions = jwt.Permissions{}
+
+		token, err := ValidateAndSign(claims, a.issuerKeyPair)
+		a.Respond(r, userNkey, serverId, token, err)
+		return
+	} else {
+		// Try to get a google JWT from the token field
+		googleJWT := rc.ConnectOptions.Token
+		gclaims, err := a.VerifyGoogleJWT(context.Background(), googleJWT)
+		if err != nil {
+			a.Respond(r, userNkey, serverId, "", err)
+		}
+
+		log.Printf("google claims: %+v", gclaims)
+
+		// Assign JWT permissions based off the
+		a.AssignPermissions(gclaims, claims)
+
+		token, err := ValidateAndSign(claims, a.issuerKeyPair)
+		a.Respond(r, userNkey, serverId, token, err)
+	}
+}
+
+func (a *AuthService) Respond(req micro.Request, userNKey, serverId, userJWT string, err error) {
+	rc := jwt.NewAuthorizationResponseClaims(userNKey)
+	rc.Audience = serverId
+	rc.Jwt = userJWT
+	if err != nil {
+		rc.Error = err.Error()
+	}
+
+	token, err := rc.Encode(a.issuerKeyPair)
+	if err != nil {
+		log.Println("error encoding response jwt:", err)
+	}
+
+	req.Respond([]byte(token))
+}
+
+func ValidateAndSign(claims *jwt.UserClaims, kp nkeys.KeyPair) (string, error) {
+	// Validate the claims.
+	vr := jwt.CreateValidationResults()
+	claims.Validate(vr)
+	if len(vr.Errors()) > 0 {
+		return "", errors.Join(vr.Errors()...)
+	}
+
+	// Sign it with the issuer key since this is non-operator mode.
+	return claims.Encode(kp)
+}
+
+func (a *AuthService) VerifyGoogleJWT(ctx context.Context, token string) (*GoogleClaims, error) {
+	claims := &GoogleClaims{}
+
+	idToken, err := a.verifier.Verify(ctx, token)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := idToken.Claims(claims); err != nil {
+		return nil, err
+	}
+
+	return claims, nil
+}
+
+func (a *AuthService) AssignPermissions(gclaims *GoogleClaims, uc *jwt.UserClaims) {
+	userId := base64.StdEncoding.EncodeToString([]byte(gclaims.Email))
+	uc.Name = userId
+
+	uc.Permissions = jwt.Permissions{
+		Pub: jwt.Permission{
+			Allow: jwt.StringList{
+				"$JS.API.INFO", // General JS Info
+
+				// Chat permisions
+				fmt.Sprintf("chat.*.%s", userId),            // Publishing chat messages for this user id
+				"$JS.API.STREAM.INFO.chat_messages",         // Getting info on chat_messages stream
+				"$JS.API.CONSUMER.CREATE.chat_messages.>",   // Creating consumers on chat_messages stream
+				"$JS.API.CONSUMER.MSG.NEXT.chat_messages.>", // Creating consumers on chat_messages stream
+
+				// Workspace permissions
+				"$JS.API.DIRECT.GET.KV_chat_workspace.>",        // Gets from workspace KV
+				"$JS.API.STREAM.INFO.KV_chat_workspace",         // Info about workspace KV
+				"$JS.API.CONSUMER.CREATE.KV_chat_workspace.*.>", // Creating consumers/watchers on workspace KV
+			},
+		},
+		// Sub:  jwt.Permission{},
+	}
+}
--- 19-auth-callout/auth-service/main.go
+++ 19-auth-callout/auth-service/main.go
@@ -2,121 +2,25 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
-	"runtime"
 
-	"github.com/nats-io/jwt/v2"
-	"github.com/nats-io/nats.go"
-	"github.com/nats-io/nats.go/micro"
 	"github.com/nats-io/nkeys"
 )
 
 // Warning! You will want this in a secret store
 // of some kind instead of the source code like this
 const NKeySeed string = "SAAIRUPUPZ4CZZX4EYX2MF6A3KN7FGI3AQPEVF3HI2JXVNV6DJGSTZGDFE"
-
-var issuerKeypair nkeys.KeyPair
+const ClientID = "476732082534-gpp6r1t67lirjfddjckm81vr6c2kikr1.apps.googleusercontent.com"
 
 func main() {
-	err := RunAuthService()
+	kp, err := nkeys.FromSeed([]byte(NKeySeed))
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func RunAuthService() error {
-	err := InitGoogleOIDC(context.Background())
+	auth := NewAuthService(kp, ClientID, "cli", "my-password")
+	err = auth.Run(context.Background())
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-
-	nc, err := nats.Connect(nats.DefaultURL, nats.UserInfo("auth", "auth"))
-	if err != nil {
-		return err
-	}
-
-	issuerKeypair, err = nkeys.FromSeed([]byte(NKeySeed))
-	if err != nil {
-		return err
-	}
-
-	_, err = micro.AddService(nc, micro.Config{
-		Name:        "auth",
-		Version:     "0.0.1",
-		Description: "Handle authorization of Google JWTs for chat applications",
-		Endpoint: &micro.EndpointConfig{
-			Subject: "$SYS.REQ.USER.AUTH",
-			Handler: micro.HandlerFunc(AuthHandler),
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	log.Println("listening on $SYS.REQ.USER.AUTH")
-	runtime.Goexit()
-	return nil
-}
-
-func AuthHandler(r micro.Request) {
-	log.Println("Received Request")
-
-	rc, err := jwt.DecodeAuthorizationRequestClaims(string(r.Data()))
-	if err != nil {
-		log.Println("Error", err)
-		r.Error("500", err.Error(), nil)
-	}
-
-	userNkey := rc.UserNkey
-	serverId := rc.Server.ID
-	claims := jwt.NewUserClaims(rc.UserNkey)
-
-	// this gives me a backdoor with the CLI. Don't do this in production!
-	if rc.ConnectOptions.Username == "cli" && rc.ConnectOptions.Password == "my-password" {
-		claims.Name = rc.ConnectOptions.Username
-		claims.Audience = "$G"
-		claims.Permissions = jwt.Permissions{}
-
-		token, err := ValidateAndSign(claims, issuerKeypair)
-		Respond(r, userNkey, serverId, token, err)
-		return
-	} else {
-		// Try to get a google JWT from the token field
-		googleJWT := rc.ConnectOptions.Token
-		gclaims, err := VerifyGoogleJWT(context.Background(), googleJWT)
-		if err != nil {
-			Respond(r, userNkey, serverId, "", err)
-		}
-
-		log.Printf("google claims: %+v", gclaims)
-	}
-}
-
-func Respond(req micro.Request, userNKey, serverId, userJWT string, err error) {
-	rc := jwt.NewAuthorizationResponseClaims(userNKey)
-	rc.Audience = serverId
-	rc.Jwt = userJWT
-	if err != nil {
-		rc.Error = err.Error()
-	}
-
-	token, err := rc.Encode(issuerKeypair)
-	if err != nil {
-		log.Println("error encoding response jwt:", err)
-	}
-
-	req.Respond([]byte(token))
-}
-
-func ValidateAndSign(claims *jwt.UserClaims, kp nkeys.KeyPair) (string, error) {
-	// Validate the claims.
-	vr := jwt.CreateValidationResults()
-	claims.Validate(vr)
-	if len(vr.Errors()) > 0 {
-		return "", errors.Join(vr.Errors()...)
-	}
-
-	// Sign it with the issuer key since this is non-operator mode.
-	return claims.Encode(kp)
 }
