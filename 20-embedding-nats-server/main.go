a44cc68c82a4118d3816b7c1588c984d296c66e2
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-07-08 15:41:33 -0700

    Updated with leaf node

--- 20-embedding-nats-server/main.go
+++ 20-embedding-nats-server/main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+	"log"
+	"net/url"
+	"time"
+
 	"github.com/nats-io/nats-server/v2/server"
 	"github.com/nats-io/nats.go"
-	"time"
 )
 
 func main() {
+	nc, ns, err := RunEmbeddedServer(true, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nc.Subscribe("hello.world", func(msg *nats.Msg) {
+		log.Println("message received!")
+		msg.Respond([]byte("Ahoy there!"))
+	})
+
+	ns.WaitForShutdown()
 }
 
 func RunEmbeddedServer(inProcess bool, enableLogging bool) (*nats.Conn, *server.Server, error) {
+	leafURL, err := url.Parse("nats-leaf://connect.ngs.global")
+	// leafURL, err := url.Parse("nats-leaf://0.0.0.0:7422")
+	if err != nil {
+		return nil, nil, err
+	}
+
 	opts := &server.Options{
-		DontListen: inProcess,
+		ServerName:      "embedded_server",
+		DontListen:      inProcess,
+		JetStream:       true,
+		JetStreamDomain: "embedded",
+		LeafNode: server.LeafNodeOpts{
+			Remotes: []*server.RemoteLeafOpts{
+				{
+					URLs:        []*url.URL{leafURL},
+					Credentials: "./leafnode.creds",
+				},
+			},
+		},
 	}
 
 	ns, err := server.NewServer(opts)
